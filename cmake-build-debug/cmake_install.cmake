f6a11856faaa97a415fe23a965d593fab211e886
gahan9 <gahansaraiya@gmail.com>
2018-08-17 19:06:09 +0530

    added cmake files

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/mtechcse/gahan/CA_lab
+# Install script for directory: /home/jarvis/Projects/LAB_nirmauni/CA_lab
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/mtechcse/gahan/CA_lab/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/jarvis/Projects/LAB_nirmauni/CA_lab/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
